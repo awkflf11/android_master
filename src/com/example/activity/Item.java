b8bf1c8a116ba6a3b4cea27219d6a85b063360a2
wkf <wangkunfeng@foryou56.com>
2016-10-17 15:28:28 +0800

    no message

---
+++ src/com/example/activity/Item.java
@@ -0,0 +1,35 @@
+package com.example.activity;
+
+public class Item {
+	
+	public Item(){}
+	
+	private boolean isSelect;
+	private int id;
+	/**
+	 * @return the isSelect
+	 */
+	public boolean isSelect() {
+		return isSelect;
+	}
+	/**
+	 * @param isSelect the isSelect to set
+	 */
+	public void setSelect(boolean isSelect) {
+		this.isSelect = isSelect;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
