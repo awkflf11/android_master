667981a3b6ae541306bcb15af7c046692e75f5bf
awkflf11 <wkflf1988@126.com>
2016-10-18 09:44:37 +0800

    dd

--- mainActivity/src/main/java/com/foryou/truck/entity/CryptEntity.java
+++
@@ -1,8 +0,0 @@
-package com.foryou.truck.entity;
-
-/**
- * Created by dubin on 16/5/25.
- */
-public class CryptEntity {
-    public String fydata;
-}
--- mainActivity/src/main/java/com/foryou/truck/entity/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.foryou.truck.entity;
-
-public class OrderStatus {
-	public String key; // 10:未成单，待支付 9:报价收集中 7:等待审核 15:已到达
-	public String text;
-}
